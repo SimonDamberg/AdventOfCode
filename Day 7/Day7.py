78f90017484ccbbe92d56d69f109f1f54aa285df
Simon Damberg <damberg.simon00@gmail.com>
2020-12-14 23:59:28 +0100

    Day 7 Part 1

--- Day 7/Day7.py
+++ Day 7/Day7.py
@@ -1,29 +1,66 @@
-with open("input.txt") as file:
-    lines = file.readlines()
 
-rules = {}
-occurences = []
 
-#Counts as 2 ways if a bag contains 2 other bags that contains a shiny gold one
-def getContents(bag):
+def parsePart1():
+    rules = {}
+    with open("input.txt") as file:
+        lines = file.readlines()
+    for line in lines:
+        currentBag = " ".join(line.split(" ")[:2])
+        contents = line.split("contain")[1].split(",")
+        colours = []
+        for bag in contents:
+            colours.append(" ".join(bag.split(" ")[2:4]))
+        rules[currentBag] = colours
+    return rules
+
+
+def parsePart2():
+    rules = {}
+    with open("test.txt") as file:
+        lines = file.readlines()
+    for line in lines:
+        currentBag = " ".join(line.split(" ")[:2])
+        contents = line.split("contain")[1].split(",")
+        subBags = {}
+        for bag in contents:
+            if(bag[1].isdigit()):
+                subBags[" ".join(bag.split(" ")[2:4])] = bag[1]
+        rules[currentBag] = subBags
+    return rules
+
+def containsShinyGold(bag):
     try:
-        content = rules[bag]
-        for result in content:
-            if "shiny gold" in result:
-                occurences.append(result)
-            else:
-                getContents(result)
+        subBags = rules[bag]
+        for subBag in subBags:
+            if subBag == "shiny gold" or containsShinyGold(subBag):
+                return True
+        return False
     except:
-        pass
+        return False
 
-for line in lines:
-    currentBag = " ".join(line.split(" ")[:2])
-    contents = line.split("contain")[1].split(",")
-    colours = []
-    for bag in contents:
-        colours.append(" ".join(bag.split(" ")[2:4]))
-    rules[currentBag] = colours
+def totalBagsInside(bag):
+    try:
+        subBags = rules[bag]
+        print(str(bag) + ": " + str(subBags))
+        count = 0
+        for colour, amount in subBags.items():
+            count = count + (int(amount)*totalBagsInside(colour))
+        return count
+            #count += v #+ totalBagsInside(sub)
+    except:
+        return 0
 
-for rule in rules:
-    getContents(rule)
-print(len(occurences))
+
+#Part 1
+rules = parsePart1()
+part1 = 0
+for bag in rules:
+    if containsShinyGold(bag):
+        part1 += 1
+
+#Part 2
+rules = parsePart2()
+part2 = totalBagsInside("shiny gold")
+
+print("Part 1: " + str(part1))
+print("Part 2: " + str(part2))
