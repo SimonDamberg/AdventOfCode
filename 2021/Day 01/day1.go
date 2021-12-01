5230ade73e38d25d84a0f7bb5a12e656391782c7
Simon Damberg <damberg.simon00@gmail.com>
2021-12-01 08:08:53 +0100

    Day 1 complete

--- 2021/Day 01/day1.go
+++ 2021/Day 01/day1.go
@@ -1,19 +1,52 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
 	"os"
+	"strconv"
 )
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
-
 func main() {
+	file, err := os.Open("day1.txt")
+	defer file.Close()
 
-	data, error := os.ReadFile("day1.txt")
-	check(error)
-	fmt.Print(string(data))
+	if err != nil {
+		log.Fatalf("readLines: %s", err)
+	}
+
+	var lines []int
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		val, err1 := strconv.Atoi(scanner.Text())
+		if err1 != nil {
+			log.Fatalf("readLines: %s", err)
+		}
+		lines = append(lines, val)
+	}
+
+	var prev1 int = 0
+	var prev2 int = 0
+	var prev3 int = 0
+
+	var noIncreases int = 0
+	var noIncreases2 int = 0
+	for i, v := range lines {
+		if i > 0 {
+			if v > lines[i-1] {
+				noIncreases++
+			}
+		}
+		if i >= 3 {
+			if v+prev2+prev3 > prev1+prev2+prev3 {
+				noIncreases2++
+			}
+		}
+		prev1 = prev2
+		prev2 = prev3
+		prev3 = v
+	}
+	fmt.Printf("Part1: %d\n", noIncreases)
+	fmt.Printf("Part2: %d", noIncreases2)
 }
