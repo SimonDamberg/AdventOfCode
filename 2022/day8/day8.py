dbedd6b237c672132647a6450ec4306d39f7814d
Simon Damberg <damberg.simon00@gmail.com>
2022-12-08 09:20:02 +0100

    🧹day 8 cleanup

--- 2022/day8/day8.py
+++ 2022/day8/day8.py
@@ -7,51 +7,31 @@ with open('input.txt') as f:
         for y in range(len(lines)):
             grid[x, y] = int(lines[x][y])
 
-visible = 2 * (grid.shape[0] + grid.shape[1] - 2)
-for i in range(1, grid.shape[0] - 1):
-    for j in range(1, grid.shape[1] - 1):
-        height = grid[i, j]
-
-        if np.all(grid[i, j + 1:] < height):
-            visible += 1
-        elif np.all(grid[i, :j] < height):
-            visible += 1
-        elif np.all(grid[i + 1:, j] < height):
-            visible += 1
-        elif np.all(grid[:i, j] < height):
-            visible += 1
-
-print(f"Part 1: {visible}")
-
+visible = 2 * (grid.shape[0] + grid.shape[1] - 2) # All edges are visible by default
 best_scenic = -np.inf
 for i in range(1, grid.shape[0]-1):
     for j in range(1, grid.shape[1]-1):
         height = grid[i, j]
+
+        # Part 1
+        if np.any([np.all(grid[i, j + 1:] < height), np.all(grid[i, :j] < height), np.all(grid[i + 1:, j] < height), np.all(grid[:i, j] < height) > 0]):
+            visible += 1
+
+        # Part 2: Check all directions
         scenic = 1
-
-        # Check both x directions
-        for x_dir in (-1, 1):
+        for x_dir, y_dir in ((-1, 0), (1, 0), (0, -1), (0, 1)):
             x = i + x_dir
-            view = 0
-            while 0 <= x < grid.shape[0]:
-                view += 1
-                if grid[x, j] >= height:
-                    break
-                x += x_dir
-            scenic = scenic * view
-
-        # Check both y directions
-        for y_dir in (-1, 1):
             y = j + y_dir
             view = 0
-            while 0 <= y < grid.shape[1]:
+            while 0 <= x < grid.shape[0] and 0 <= y < grid.shape[1]:
                 view += 1
-                if grid[i, y] >= height:
+                if grid[x, y] >= height:
                     break
+                x += x_dir
                 y += y_dir
             scenic = scenic * view
-        
         best_scenic = max(best_scenic, scenic)
 
+print(f"Part 1: {visible}")
 print(f"Part 2: {best_scenic}")
 
