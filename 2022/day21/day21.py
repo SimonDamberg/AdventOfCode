7fc9e381bc2357de565c831be28250521099db71
Simon Damberg <damberg.simon00@gmail.com>
2023-12-01 12:56:29 +0100

    fixed day21 args

--- 2022/day21/day21.py
+++ 2022/day21/day21.py
@@ -13,28 +13,28 @@ def parse_file():
                 waiting[monkey] = yelling
     return nums, waiting, root_1, root_2
 
-def get_value(monkey):
+def get_value(monkey, nums, waiting):
     if monkey in nums:
         return nums[monkey]
     else:
         a, op, b = waiting[monkey].split(" ")
-        nums[monkey] = eval(f"{get_value(a)} {op} {get_value(b)}")
+        nums[monkey] = eval(f"{get_value(a, nums, waiting)} {op} {get_value(b, nums, waiting)}")
         return nums[monkey]
 
 nums, waiting, root_1, root_2 = parse_file()
-print(f"Part 1: {int(get_value(root_1) + get_value(root_2))}")
+print(f"Part 1: {int(get_value(root_1, nums, waiting) + get_value(root_2, nums, waiting))}")
 
-def is_humn_in_sequence(root):
+def is_humn_in_sequence(root, waiting):
     if root == "humn":
         return 1
     else:
         found = 0
         if root in waiting:
             for node in [waiting[root].split(" ")[0], waiting[root].split(" ")[2]]:
-                found += is_humn_in_sequence(node)
+                found += is_humn_in_sequence(node, waiting)
         return found
 
-def find_humn(wanted, monkey):
+def find_humn(wanted, monkey, nums, waiting):
     if monkey == "humn":
         return wanted
     elif monkey in nums:
@@ -42,30 +42,30 @@ def find_humn(wanted, monkey):
     else:
         a, op, b = waiting[monkey].split(" ")
         if op == "+":
-            if is_humn_in_sequence(a):
-                nums[monkey] = find_humn(wanted-get_value(b), a)
+            if is_humn_in_sequence(a, waiting):
+                nums[monkey] = find_humn(wanted-get_value(b, nums, waiting), a, nums, waiting)
             else:
-                nums[monkey] = find_humn(wanted-get_value(a), b)
+                nums[monkey] = find_humn(wanted-get_value(a, nums, waiting), b, nums, waiting)
         elif op == "-":
-            if is_humn_in_sequence(a):
-                nums[monkey] = find_humn(wanted+get_value(b), a)
+            if is_humn_in_sequence(a, waiting):
+                nums[monkey] = find_humn(wanted+get_value(b, nums, waiting), a, nums, waiting)
             else:
-                nums[monkey] = find_humn(get_value(a)-wanted, b)
+                nums[monkey] = find_humn(get_value(a, nums, waiting)-wanted, b, nums, waiting)
         elif op == "*":
-            if is_humn_in_sequence(a):
-                nums[monkey] = find_humn(wanted/get_value(b), a)
+            if is_humn_in_sequence(a, waiting):
+                nums[monkey] = find_humn(wanted/get_value(b, nums, waiting), a, nums, waiting)
             else:
-                nums[monkey] = find_humn(wanted/get_value(a), b)
+                nums[monkey] = find_humn(wanted/get_value(a, nums, waiting), b, nums, waiting)
         else:
-            if is_humn_in_sequence(a):
-                nums[monkey] = find_humn(wanted*get_value(b), a)
+            if is_humn_in_sequence(a, waiting):
+                nums[monkey] = find_humn(wanted*get_value(b, nums, waiting), a, nums, waiting)
             else:
-                nums[monkey] = find_humn(get_value(a)/wanted, b)
+                nums[monkey] = find_humn(get_value(a, nums, waiting)/wanted, b, nums, waiting)
         return nums[monkey]
 
 nums, waiting, root_1, root_2 = parse_file()
-if is_humn_in_sequence(root_1):
-    wanted, root = get_value(root_2), root_1
+if is_humn_in_sequence(root_1, waiting):
+    wanted, root = get_value(root_2, nums, waiting), root_1
 else:
-    wanted, root = get_value(root_1), root_2
-print(f"Part 2: {int(find_humn(wanted, root))}")
+    wanted, root = get_value(root_1, nums, waiting), root_2
+print(f"Part 2: {int(find_humn(wanted, root, nums, waiting))}")
